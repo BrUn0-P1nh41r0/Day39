b57ee01f09c86f13e9a4e05ea163941a5aaddd27
Bruno <141152582+BrUn0-P1nh41r0@users.noreply.github.com>
2025-02-21 18:44:10 +0000

    final project

--- flight_search.py
+++ flight_search.py
@@ -1,6 +1,101 @@
+import os
+import requests
+from datetime import datetime
+from dotenv import load_dotenv
+
+load_dotenv()
+
+AMADEUS_TOKEN_ENDPOINT = os.environ["AMADEUS_TOKEN_ENDPOINT"]
+AMADEUS_IATA_ENDPOINT = os.environ["AMADEUS_IATA_ENDPOINT"]
+AMADEUS_FLIGHT_ENDPOINT = os.environ["AMADEUS_FLIGHT_ENDPOINT"]
+
 class FlightSearch:
     #This class is responsible for talking to the Flight Search API.
 
+    def __init__(self):
+        self._api_key = os.environ["AMADEUS_API_KEY"]
+        self._api_secret = os.environ["AMADEUS_API_SECRET"]
+        self._token = self.get_new_token()
+
+    def get_new_token(self):
+        header = {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+        body = {
+            'grant_type': 'client_credentials',
+            'client_id': self._api_key,
+            'client_secret': self._api_secret
+        }
+
+        response = requests.post(url=AMADEUS_TOKEN_ENDPOINT, headers=header, data=body)
+        return response.json()["access_token"]
+
     def get_iatacode(self, city_name):
-        code = "Testing"
+        print(f"Using this token to get destination {self._token}")
+        headers = {"Authorization": f"Bearer {self._token}"}
+        query = {
+            "keyword": city_name,
+            "max":"2",
+            "include":"AIRPORTS",
+        }
+
+        response = requests.get(url=AMADEUS_IATA_ENDPOINT, headers=headers, params=query)
+
+        try:
+            code = response.json()["data"][0]["iataCode"]
+        except IndexError:
+            print(f"IndexError: No airport code found for: {city_name}")
+            return "N/A"
+        except KeyError:
+            print(f"KeyError: No airport code found for: {city_name}")
+            return "Not Found"
+
         return code
+
+    def check_flights(self, origin_city_code, destination_city_code, from_time, to_time):
+        """
+        Searches for flight options between two cities on specified departure and return dates
+        using the Amadeus API.
+        Parameters:
+            origin_city_code (str): The IATA code of the departure city.
+            destination_city_code (str): The IATA code of the destination city.
+            from_time (datetime): The departure date.
+            to_time (datetime): The return date.
+        Returns:
+            dict or None: A dictionary containing flight offer data if the query is successful; None
+            if there is an error.
+        The function constructs a query with the flight search parameters and sends a GET request to
+        the API. It handles the response, checking the status code and parsing the JSON data if the
+        request is successful. If the response status code is not 200, it logs an error message and
+        provides a link to the API documentation for status code details.
+        """
+
+        # print(f"Using this token to check_flights() {self._token}")
+        headers = {"Authorization": f"Bearer {self._token}"}
+        query = {
+            "originLocationCode": origin_city_code,
+            "destinationLocationCode": destination_city_code,
+            "departureDate": from_time.strftime("%Y-%m-%d"),
+            "returnDate": to_time.strftime("%Y-%m-%d"),
+            "adults": 1,
+            "nonStop": "true",
+            "currencyCode": "GBP",
+            "max": "10",
+        }
+
+        response = requests.get(
+            url=AMADEUS_FLIGHT_ENDPOINT,
+            headers=headers,
+            params=query,
+        )
+
+        if response.status_code != 200:
+            print(f"check_flights() response code: {response.status_code}")
+            print("There was a problem with the flight search.\n"
+                  "For details on status codes, check the API documentation:\n"
+                  "https://developers.amadeus.com/self-service/category/flights/api-doc/flight-offers-search/api"
+                  "-reference")
+            print("Response body:", response.text)
+            return None
+
+        return response.json()
--- main.py
+++ main.py
@@ -1,25 +1,51 @@
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
-from data_manager import DataManager
-import flight_data
-from flight_search import FlightSearch
 import notification_manager
-import os
-from dotenv import load_dotenv
-
-#Load environment variables from .env file
-load_dotenv()
-
-AMADEUS_API_KEY = os.environ["AMADEUS_API_KEY"]
-AMADEUS_API_SECRET = os.environ["AMADEUS_API_SECRET"]
+import time
+from datetime import datetime, timedelta
+from flight_search import FlightSearch
+from data_manager import DataManager
+from flight_data import find_cheapest_flight
 
 data_manager = DataManager()
 sheet_data = data_manager.get_destination_data()
+flight_search = FlightSearch()
 
-if sheet_data[0]["iataCode"] == "":
-    flight_search = FlightSearch()
-    for row in sheet_data:
+ORIGIN_CITY_IATA = "LON"
+
+for row in sheet_data:
+    if row["iataCode"] == "":
         row["iataCode"] = flight_search.get_iatacode(row["city"])
-    print(f"sheet_data: \n{sheet_data}")
+        time.sleep(2)
 
-    data_manager.destination_data = sheet_data
-    data_manager.update_destination_codes()
+data_manager.destination_data = sheet_data
+data_manager.update_destination_codes()
+
+tomorrow = datetime.now() + timedelta(days=1)
+six_month_from_today = datetime.now() + timedelta(days=(6 * 30))
+
+for destination in sheet_data:
+    print(f"Getting flights for {destination['city']}...")
+    flights = flight_search.check_flights(
+        ORIGIN_CITY_IATA,
+        destination["iataCode"],
+        from_time=tomorrow,
+        to_time=six_month_from_today
+    )
+    cheapest_flight = find_cheapest_flight(flights)
+    print(f"{destination['city']}: £{cheapest_flight.price}")
+    # Slowing down requests to avoid rate limit
+    time.sleep(2)
+    if cheapest_flight.price != "N/A" and cheapest_flight.price < destination["lowestPrice"]:
+        print(f"Lower price flight found to {destination['city']}!")
+        # notification_manager.send_sms(
+        #     message_body=f"Low price alert! Only £{cheapest_flight.price} to fly "
+        #                  f"from {cheapest_flight.origin_airport} to {cheapest_flight.destination_airport}, "
+        #                  f"on {cheapest_flight.out_date} until {cheapest_flight.return_date}."
+        # )
+
+        # SMS not working? Try whatsapp instead.
+        notification_manager.NotificationManager.send_whatsapp(
+            message_body=f"Low price alert! Only £{cheapest_flight.price} to fly "
+                         f"from {cheapest_flight.origin_airport} to {cheapest_flight.destination_airport}, "
+                         f"on {cheapest_flight.out_date} until {cheapest_flight.return_date}."
+        )
