0e44fb5aa6e1400e69765b7eab61e38002db8b86
Bruno <141152582+BrUn0-P1nh41r0@users.noreply.github.com>
2025-02-21 16:37:41 +0000

    use sheeety to read and write data

--- flight_search.py
+++ flight_search.py
@@ -1,3 +1,6 @@
 class FlightSearch:
     #This class is responsible for talking to the Flight Search API.
-    pass
+
+    def get_iatacode(self, city_name):
+        code = "Testing"
+        return code
